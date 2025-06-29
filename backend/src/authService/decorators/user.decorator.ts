83615929b5d4df9c2c6f06426938fcd2b0b93561
Victor Trimpai <victor.trimpai1987@gmail.com>
2025-06-29 18:42:45 -0400

    vTrimpai: Se mejora roles y permisos, se agregan nuevos endpoint

--- backend/src/authService/decorators/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-// src/common/decorators/user.decorator.ts
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const User = createParamDecorator(
-  (data, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user; // viene del JWT después del AuthGuard
-  },
-);
--- backend/src/authService/guards/jwt-auth.guard.ts
+++ backend/src/authService/guards/jwt-auth.guard.ts
@@ -1,13 +1,28 @@
 // src/authService/jwt-auth.guard.ts
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Verifica si el endpoint tiene el decorador @Public()
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const token = request.cookies['access_token'];
 
